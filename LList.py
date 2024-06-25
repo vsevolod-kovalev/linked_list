c81c4aaa86ab549a06c065f9077b188747b0c859
bapuliz <87442802+bapuliz@users.noreply.github.com>
2024-06-25 11:24:01 -0700

    style: reformat code

--- LList.py
+++ LList.py
@@ -9,65 +9,64 @@ class LList:
 
     def append(self, data):
         newNode = Node(data)
-        if self.head == None:
+        if self.head is None:
             self.head = newNode
             return
         t_head = self.head
-        while (t_head.next != None):
+        while (t_head.next is not None):
             t_head = t_head.next
         t_head.next = newNode
 
     def prepend(self, data):
         newNode = Node(data)
-        if (self.head != None):
-            newNode.next = self.head
+        newNode.next = self.head
         self.head = newNode
 
     def insert_after_node(self, prev_node, data):
-        if (prev_node == None):
-            raise Exception("Cannot insert after None")
+        if (prev_node is None):
+            raise ValueError("Previous node cannot be None")
         newNode = Node(data)
         t_head = self.head
-        while (t_head != prev_node):
+        while (t_head is not prev_node):
+            if (t_head is None):
+                raise ValueError("Specified node is not in the list")
             t_head = t_head.next
         newNode.next = prev_node.next
         prev_node.next = newNode
 
     def delete_node(self, key):
-        if (key == None):
+        if (key is None):
             raise ValueError("Cannot delete a node with None data")
         t_head = self.head
-        if (t_head == None):
-            raise Exception("List is empty")
+        if (t_head is None):
+            raise ValueError("List is empty")
         if (t_head.data == key):
             self.head = self.head.next
             return
-        while (t_head.next != None):
+        while (t_head.next is not None):
             if (t_head.next.data == key):
                 t_head.next = t_head.next.next
                 return
             t_head = t_head.next
-        raise Exception("Node with the specified key does not exist")
+        raise ValueError("Node with the specified key does not exist")
 
     def delete_node_at_pos(self, pos):
-        if not isinstance(pos, int):
-            raise ValueError("Position must be an integer")
-        if pos < 0:
-            raise ValueError("Position must be a non-negative integer")
-        if (self.head == None):
-            raise Exception("List is empty")
-        if (pos == 0):
+        if not isinstance(pos, int) or pos < 0:
+            raise ValueError("Position must be an non-negative integer")
+        if self.head is None:
+            raise ValueError("List is empty")
+        if pos == 0:
             self.head = self.head.next
             return
         t_head = self.head
         current_index = 0
-        while current_index != pos-1 and t_head.next != None:
+        while current_index != pos-1 and t_head.next is not None:
             t_head = t_head.next
             current_index += 1
-        if (t_head.next != None):
+        if (t_head.next is not None):
             t_head.next = t_head.next.next
             return
-        raise Exception("Position is out of bounds")
+        raise IndexError("Position is out of bounds")
 
     def search(self, key):
         t_head = self.head
@@ -75,12 +74,12 @@ class LList:
             if (t_head.data == key):
                 return t_head
             t_head = t_head.next
-        raise Exception("Node with the specified key does not exist")
+        raise ValueError("Node with the specified key does not exist")
 
     def get_length(self):
         length = 0
         t_head = self.head
-        while (t_head != None):
+        while (t_head is not None):
              t_head = t_head.next
              length += 1
         return length
@@ -108,8 +107,8 @@ class LList:
         self.head = prev
 
     def middle(self):
-        if (self.head == None):
-            raise Exception("List is empty")
+        if (self.head is None):
+            raise ValueError("List is empty")
         slow = self.head
         fast = self.head
         while not (fast is None or fast.next is None):
@@ -132,7 +131,7 @@ class LList:
 
     def print(self):
         t_head = self.head
-        while (t_head != None):
+        while (t_head is not None):
             print(t_head.data)
             t_head = t_head.next
 
@@ -152,15 +151,12 @@ l.append(2)
 l.append(3)
 l.append(4)
 l.append(5)
-# l.prepend(10)
-# l.insert_after_node(l.head, 99)
-# l.insert_after_node(l.search(3), 100)
+l.prepend(10)
 l.print()
 print("\n")
 l.reverse()
 l.print()
 print()
 print(l.middle().data)
-
-# print(l.get_length())
+print(l.get_length())
 
